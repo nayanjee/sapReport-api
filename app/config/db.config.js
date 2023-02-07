17d32021a42fc1353c7d921c1f6bd160722f7b41
unknown <Nayan.Prakash@LARENON.NET>
2023-02-07 17:18:43 +0530

    config file

---
+++ app/config/db.config.js
@@ -0,0 +1,17 @@
+/**
+    * @description      : 
+    * @author           : nayan.prakash
+    * @group            : 
+    * @created          : 04/06/2022 - 15:05:41
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 04/06/2022
+    * - Author          : nayan.prakash
+    * - Modification    : 
+**/
+module.exports = {
+  HOST: "localhost",
+  PORT: 27017,
+  DB: "larenon-master"
+};
